de22f2b69a776fc5bc4b5ad0597a5d7316ee768d
Horther <Felipe.Venancio-Silva@outlook.com>
2024-05-02 19:48:46 +0100

    Configurações terminadas

--- script.js
+++ script.js
@@ -2,26 +2,42 @@ const form = document.getElementById('formulario');
 const campoA = document.getElementById('num_menor');
 const campoB = document.getElementById('num_maior');
 const message = document.getElementById('message');
-const campo = document.querySelector('') //apagar a div da mensagem quando estiver editando os campos
+
+
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
     if (campoA.value === '' || campoB.value === '')
     {
-        message.style.display = "block";
-        message.innerHTML = `Adicione algo aos campos para comparar`
-    } else if(campoA.value >= campoB.value)
+        showMessage (`Adicione algo aos campos para comparar`);
+        message.classList.add('error');
+    } else if (campoA.value == campoB.value)
         {
-        console.log(campoA.value.type)
-        message.style.display = 'block';
-        message.innerHTML = `O Campo A (${campoA.value}) é <span>MAIOR</span> que o Campo B (${campoB.value}), por favor alterar os valores`;
+            showMessage(`Os valores são iguais, por favor siga a instrução do titulo`)
+            message.classList.add('error');
+        } else if(campoA.value >= campoB.value)
+            {
+        showMessage(`O Campo A (${campoA.value}) é <span>MAIOR</span> que o Campo B (${campoB.value}), por favor alterar os valores`);
+        message.classList.add('error');
+
         }   else {
-        message.style.display = 'block';
-        message.innerHTML = `Parabéns! O Campo A (${campoA.value}) é <span>MENOR</span> que o Campo B (${campoB.value})`;
+        showMessage(`Parabéns! O Campo A (${campoA.value}) é <span>MENOR</span> que o Campo B (${campoB.value})`);
+        message.classList.add('success');
+
     }
 
-    
+    function showMessage(msg) {
+        message.innerHTML = msg;
+        message.style.display = 'block';
+        setTimeout(function () {
+            message.style.display = 'none';
+
+            message.classList.remove('error');
+            message.classList.remove('success');
+
+        }, 3000);
+    }
 })
 
 
