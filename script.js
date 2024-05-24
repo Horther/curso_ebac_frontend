3574b21ebbc541162ef1dcbaafc7e6bdadda979c
Horther <Felipe.Venancio-Silva@outlook.com>
2024-05-24 15:20:40 +0100

    Primeiro commit

--- script.js
+++ script.js
@@ -1,29 +1,20 @@
-const form = document.getElementById('formulario');
-const campoA = document.getElementById('num_menor');
-const campoB = document.getElementById('num_maior');
-const message = document.getElementById('message');
-const campo = document.querySelector('') //apagar a div da mensagem quando estiver editando os campos
+$(document).ready(function (){
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    $('form').on('submit',function(e){
+        e.preventDefault();
+        const nomeTarefa = $('#nome-tarefa').val();
+        const novaTarefa = $(`<li></li>`);
 
-    if (campoA.value === '' || campoB.value === '')
-    {
-        message.style.display = "block";
-        message.innerHTML = `Adicione algo aos campos para comparar`
-    } else if(campoA.value >= campoB.value)
-        {
-        console.log(campoA.value.type)
-        message.style.display = 'block';
-        message.innerHTML = `O Campo A (${campoA.value}) é <span>MAIOR</span> que o Campo B (${campoB.value}), por favor alterar os valores`;
-        }   else {
-        message.style.display = 'block';
-        message.innerHTML = `Parabéns! O Campo A (${campoA.value}) é <span>MENOR</span> que o Campo B (${campoB.value})`;
-    }
+        //$(`${nomeTarefa}`).appendTo(novaTarefa);
+        $(`<p>${nomeTarefa}</p>`).appendTo(novaTarefa);
+        $(novaTarefa).appendTo('ul');
+        $('#nova-tarefa').val('');
 
-    
+    })
+
+    $('p').click(function(){
+        $(`<>`)
+    })
+
+
 })
-
-
-
-
